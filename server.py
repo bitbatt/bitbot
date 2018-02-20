b78453b8dff901fd1be19da2a469b2a26b64e4a0
bitbatt <36641256+bitbatt@users.noreply.github.com>
2018-02-20 18:26:43 +0000

    Update server.py

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ import requests
 import os
 
 app = Flask(__name__)
-port = int(os.environ["PATH"])
+port = int(os.environ["PORT"])
 print(port)
 
 @app.route('/', methods=['POST'])
@@ -32,3 +32,4 @@ def errors():
   return jsonify(status=200)
 
 app.run(port=port, host="0.0.0.0")
+
